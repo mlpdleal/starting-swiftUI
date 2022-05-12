02b2b0234b0d32de40f4af4f3cc76bcfa07da25e
mlpdleal <manoellealdeveloper@gmail.com>
2022-05-12 17:07:05 -0300

    Completed day 17 of 100 days of SwiftUI

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -8,45 +8,72 @@
 import SwiftUI
 
 struct ContentView: View {
+        
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 0
+    @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
     
-    let students = ["Harry", "Hermione", "Ron"]
-    @State private var selectedStudent = "Harry"
-    @State private var tapCount = 0
-    @State private var name = ""
+    let tipPercentages = [10, 15, 20, 25, 0]
     
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
     
     var body: some View {
+        
         NavigationView{
-            
             Form {
                 Section{
-                    Text("Hello, world!")
-                }
-                
-                Group{
-                    Button("Tap Count: \(tapCount)"){
-                        tapCount += 1
-                    }
-                }
-                
-                Group{
-                    TextField("Enter your name", text: $name)
-                    Text("Your name is \(name)")
-                }
-                
-                Group{
-                    Picker("Select your student", selection: $selectedStudent){
-                        ForEach(students, id: \.self){
-                            Text($0)
+                    TextField("Amount", value: $checkAmount, format:
+                            .currency(code: Locale.current.currencyCode ?? "BRL")
+                    )
+                    .keyboardType(.decimalPad)
+                    .focused($amountIsFocused)
+                    
+                    Picker("Number of people", selection: $numberOfPeople){
+                        ForEach(2..<100){
+                            Text("\($0) people")
                         }
                     }
+                   
                 }
                 
+                Section{
+                    Picker("Tip percentage", selection: $tipPercentage){
+                        ForEach(tipPercentages, id: \.self){
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("How much tip do you want to leave?")
+                }
+                
+                Section{
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currencyCode ?? "BRL"))
+                }
             }
-            .navigationTitle("SwiftUI")
-            .navigationBarTitleDisplayMode(.inline)
+            .navigationTitle("WeSplit")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard){
+                    Spacer()
+                    Button("Done"){
+                        amountIsFocused = false
+                    }
+                }
+            }
         }
     }
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
